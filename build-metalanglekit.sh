d45497f2175abb0d1790cb37f5e6c0dc85181d62
Berstanio <berstaniodev@gmail.com>
2024-12-27 23:54:44 +0100

    fix: Try to fix pathes

--- build-metalanglekit.sh
+++ build-metalanglekit.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 rm -r build
+rm -r tmp
 mkdir build
+mkdir tmp
 
 xcodebuild archive \
     -project MetalANGLEKit.xcodeproj \
     -scheme MetalANGLEKit \
     -destination "generic/platform=iOS" \
-    -archivePath "build/MetalANGLEKit-iOS"
+    -archivePath "tmp/MetalANGLEKit-iOS"
 
 xcodebuild archive \
     -project MetalANGLEKit.xcodeproj \
     -scheme MetalANGLEKit \
     -destination "generic/platform=iOS Simulator" \
-    -archivePath "build/MetalANGLEKit-iOS_Simulator"
+    -archivePath "tmp/MetalANGLEKit-iOS_Simulator"
 
 xcodebuild -create-xcframework \
-    -archive build/MetalANGLEKit-iOS_Simulator.xcarchive -framework MetalANGLEKit.framework \
-    -archive build/MetalANGLEKit-iOS.xcarchive -framework MetalANGLEKit.framework \
+    -archive tmp/MetalANGLEKit-iOS_Simulator.xcarchive -framework MetalANGLEKit.framework \
+    -archive tmp/MetalANGLEKit-iOS.xcarchive -framework MetalANGLEKit.framework \
     -output build/MetalANGLEKit.xcframework
+
+cp -r Frameworks/* build/
